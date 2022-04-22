3681351fb070d551e479ef391029e2af000259a3
KenzoBH <brunokenzoh@hotmail.com>
2022-04-21 22:23:38 -0300

    Insert the footer via JS

---
+++ footer.js
@@ -0,0 +1,28 @@
+document.write('\
+<div id="footer-box">\
+\
+    <div id="footer">\
+        <div id="footer-infos">\
+            <div>\
+                <h2>Bruno Kenzo</h2>\
+                <p>\
+                    IME-USP, Statistics<br>\
+                    Shopee BR & IME Jr<br>\
+                    brunokenzoh@gmail.com<br>\
+                    18 yo\
+                </p>\
+            </div>\
+            <div id="footer-icons">\
+                <a href="https://www.linkedin.com/in/bruno-kenzo/" target="_blank" class="fa fa-linkedin"></a>\
+                <a href="https://github.com/KenzoBH" target="_blank" class="fa fa-github"></a>\
+                <a href="https://www.instagram.com/kenzobh/" target="_blank" class="fa fa-instagram"></a>\
+            </div>\
+        </div>\
+\
+        <div id="footer-date">\
+            <p>2022</p>\
+        </div>\
+    </div>\
+\
+</div>\
+');
